9075697320b312bd6856df534f0cd7aeb1d8004f
fayola <fayola1800018002@webmail.uad.ac.id>
2021-10-07 22:24:50 +0700

    latihan pwd 1

---
+++ unduh.php
@@ -0,0 +1,9 @@
+<?
+$myDir = "c:/xampp/htdocs/PRAK1-PWD";
+$dir = opendir($myDir);
+echo "Isi folder (klik link untuk download : <br>";
+while($tmp = readdir($dir)){
+echo "<a href='$tmp' target='_blank'>$tmp</a><br>";
+}
+closedir($dir);
+?>
